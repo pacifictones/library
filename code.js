e754880b3359975c0d45a8fd7fb2dcddec017150
pacifictones <brianwburk@gmail.com>
2022-07-07 11:52:14 +0100

    updated the book contructor to a class

--- code.js
+++ code.js
@@ -44,11 +44,13 @@ function closeModal(modal) {
 
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
+class Book {
+    constructor (title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
+    }
 }
 
 function addBookToLibrary (newBook) {
